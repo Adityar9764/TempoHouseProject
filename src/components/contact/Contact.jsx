0594b1009eaea23565de1b98ed4a8deb4e07c4ce
thesawankumar <saankushwaha249@gmail.com>
2024-08-28 12:32:12 +0530

    navbar

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
 
   return (
     <>
-      <section className="bg-white mt-[45px] dark:bg-gray-900 font-roboto">
+      <section className="bg-white mt-[30px] dark:bg-gray-900 font-roboto">
         <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
             Contact Us
