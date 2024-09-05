674e20a963efdea78f777b970a1a78bc0d444939
Aditya Raj <adityar9764@gmail.com>
2024-09-05 16:28:44 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState, useEffect } from "react";
-// import ClientCarousel from "./Clientcarousel.jsx";
 import Carousel from "./carousel.jsx";
 import WhyTrustUs from "./WhytrustUs.jsx";
 import ProductRange from "./productRange.jsx";
